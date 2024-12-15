6806ace493ec445b045e2e44ec21eaf14e7d6267
Marcos Silva <mvinicius17112002@gmail.com>
2024-12-15 12:48:13 -0300

    Início 15/12/2024

---
+++ home.js
@@ -0,0 +1,6 @@
+function coleta_de_dados(){
+    var nome = document.getElementById(`nome`).value, senha = document.getElementById(`senha`).value
+
+    alert(`credenciais sao: ${nome} ${senha}`)
+
+}
