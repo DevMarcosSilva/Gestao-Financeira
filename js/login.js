c8055914f22791aa9e4a4c7a858ecc2687200879
Marcos Silva <mvinicius17112002@gmail.com>
2024-12-16 19:14:38 -0300

    retirada

--- js/login.js
+++ js/login.js
@@ -1,7 +1,3 @@
-async function sleep(){
-    await sleep(400);
-
-}
 function coleta_de_dados(){
     var nome = document.getElementById(`first_name`).value, senha = document.getElementById(`last_name`).value
 
